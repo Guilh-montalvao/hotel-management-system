efe98b9cb68ada4f87da7025fb0c93dceacefe66
Guilherme Augusto montalvão <guilherme06075828125@gmail.com>
2025-04-25 10:25:12 -0300

    chore(deps): update project configuration
    - Update Next.js configuration
    - Update package dependencies
    - Improve build settings

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+  webpack: (config, { dev, isServer }) => {
+    // Configurações para melhorar o uso de memória
+    config.optimization.minimize = true;
 
-export default nextConfig
+    if (dev) {
+      config.optimization.runtimeChunk = "single";
+      config.optimization.removeAvailableModules = false;
+      config.optimization.removeEmptyChunks = false;
+      config.optimization.splitChunks = false;
+    }
+
+    return config;
+  },
+};
+
+export default nextConfig;
